2e15e9a24914a01a9c900f0b95f4defb46724b96
manahiljawed-mj <manahiljawed1@gmail.com>
2024-07-07 16:12:25 +0500

    Hello World Code

---
+++ Hello_World.java
@@ -0,0 +1,21 @@
+//Java is Obejct Oriented so everything should be in class
+
+class HelloWorld{
+    //When Jvm compiles and runs the java code it searches for main method
+
+    public static void main(String a[])
+    {
+        System.out.println("Hello World");
+    }
+}
+
+//To run this code first create byte code by using follow command
+
+//javac Hello_World.java
+
+
+//After this you will see a file automatically created named as HelloWorld.class
+
+//To run code use command
+
+//java HelloWorld
