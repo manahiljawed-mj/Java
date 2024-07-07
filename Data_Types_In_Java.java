c44412db92cf0e716afe1c8d58f4a73794e67978
manahiljawed-mj <manahiljawed1@gmail.com>
2024-07-07 16:26:52 +0500

    Variables and Data Types in Java

---
+++ Data_Types_In_Java.java
@@ -0,0 +1,38 @@
+import java.lang.reflect.Array;
+
+public class Data_Types_In_Java {
+
+    public static void main(String a[])
+    {
+
+        //primitive datatypes
+
+        int first=8;
+
+        long second =8l;//l is written in end
+
+        short third=8;
+
+        byte fourth=8;
+
+        char fifth='c';
+
+        float sixth=7.8f;//f to be wriiten is the end 
+
+        double seventh=8.8;
+
+        boolean eight=true;
+
+        //In non primitive  reference is stored instead of value 
+        //that is why it is also called as reference variable or object variable
+
+        //non primitive datatypes are
+        // 1.Class
+        // 2.Object
+        // 3.String
+        // 4.Array
+        // 5.Interface
+
+    }
+    
+}
---
+++ Variables_In_Java.java
@@ -0,0 +1,10 @@
+public class Variables_In_Java {
+    //to declare a variable in java the syntax is 
+    //datatype variablename = value;
+
+    public static void main(String a[])
+    {
+        int firstVariable=8;
+        System.out.println("Value of First Variable is:"+firstVariable);
+    }
+}
